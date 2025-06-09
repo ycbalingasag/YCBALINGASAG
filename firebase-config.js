8a4440ac1e924268f7fc57c79ed54f2d5222147b
ycbalingasag <ycbalingasag@gmail.com>
2025-06-10 02:49:43 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Replace with your Firebase project config from Firebase Console
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
